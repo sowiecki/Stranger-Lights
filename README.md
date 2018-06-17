87e95f981d2f26a545624fcdb452fefa33d25f16
sowiecki <nase00@gmail.com>
2018-06-17 17:14:28 -0500

    Insert example OLED code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ![demo](./demo.gif)
 
 # Hardware
-* [HiLetGo ESP8266](https://smile.amazon.com/HiLetgo-Internet-Development-Wireless-Micropython/dp/B010O1G1ES)
+* [MakerFocus ESP8266](https://smile.amazon.com/gp/product/B076JDVRLP)
 * A strand/sequence of (at least) 26 WS2812 LEDs (search for "addressable Christmas lights", make certain they are WS2812 compatible)
 * Wallpaper and black paint
 
@@ -12,6 +12,8 @@ Hang a strang of addressable LEDs, starting at the letter Z snaking up through I
 Assuming a strange of exactly 26 pixels, see the pixel the [Pixel Map](./PIXELMAP.md) for which pixel index corresponds to which letter.
 
 ### Firmware
+Follow the [board installation instructions](https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series/blob/master/InstallGuide). In the Arduino IDE, set the board type to `WIFI_Kit_8`.
+
 In [firmware/firmware.ino](./firmware/firmware.ino), set `*SSID` `*PASSWORD` to the SSID and password of your WiFi network.
 
 If your light strand doesn't correspond exactly to each letter, e.g. you have a strand of 50 lights with some being unused between each letter, set `PRECISE` to `false` and change the values in the method `lightPreciseLetter` to match your lights' positions.
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,16 @@
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <U8g2lib.h>
+
+//U8g2 Contructor
+U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 5, /* data=*/ 4, /* reset=*/ 16);
+// Alternative board version. Uncomment if above doesn't work.
+// U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 2, /* reset=*/ 4);
+
+u8g2_uint_t offset;     // current offset for the scrolling text
+u8g2_uint_t width;      // pixel width of the scrolling text (must be lesser than 128 unless U8G2_16BIT is defined
+const char *text = "STRANGER THINGS "; // scroll this text from right to left
 
 #define PIN 2 // Actually 04 on Makerfocus D1 Mini
 #define GREEN_BUTTON_PIN 14
@@ -30,6 +40,13 @@ Adafruit_NeoPixel strip =
     Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  u8g2.begin();
+
+  u8g2.setFont(u8g2_font_logisoso32_tf); // set the target font to calculate the pixel width
+  width = u8g2.getUTF8Width(text);    // calculate the pixel width of the text
+
+  u8g2.setFontMode(0);    // enable transparent mode, which is faster
+
   pinMode(GREEN_BUTTON_PIN, INPUT);
   pinMode(RED_BUTTON_PIN, INPUT);
 
@@ -43,6 +60,31 @@ void setup() {
   WiFi.begin(SSID, PASSWORD);
 
   Serial.println(" connected");
+}
+
+void display() {
+    u8g2_uint_t x;
+
+  u8g2.firstPage();
+  do {
+
+    // draw the scrolling text at current offset
+    x = offset;
+    u8g2.setFont(u8g2_font_logisoso32_tf);   // set the target font
+    do {                // repeated drawing of the scrolling text...
+      u8g2.drawUTF8(x, 32, text);     // draw the scolling text
+      x += width;           // add the pixel width of the scrolling text
+    } while ( x < u8g2.getDisplayWidth() );   // draw again until the complete display is filled
+
+    u8g2.setFont(u8g2_font_logisoso32_tf);   // draw the current pixel width
+    u8g2.setCursor(0, 64);
+    u8g2.print(width);          // this value must be lesser than 128 unless U8G2_16BIT is set
+
+  } while ( u8g2.nextPage() );
+
+  offset -= 1;            // scroll by one pixel
+  if ( (u8g2_uint_t)offset < (u8g2_uint_t) - width )
+    offset = 0;             // start over again
 }
 
 void loop() {
