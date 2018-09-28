bf6fbf785fedfca798e1dc07bcdc2ec2fc4e6c55
sowiecki <nase00@gmail.com>
2018-09-28 13:23:39 -0500

    Add button toggles.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,47 +2,68 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 
-#define PIN 2 // Actually 04 on Makerfocus D1 Mini
+#define NEOPIXEL_PIN 2      // GPIO2, D4
+#define BUTTON_PIN_GREEN 16 // GPIO16, D0
+#define BUTTON_PIN_RED 4    // GPIO0, D2
+#define DISPLAY_MESSAGE_MODE 0
+#define RAINBOW_MODE 1
 
 const bool PRECISE =
     false; // Change to true if you need to define exact light positions
-const char *SSID = "";
-const char *PASSWORD = "";
+const char *SSID = "ithurtswhenip";
+const char *PASSWORD = "hunter";
 const char *HOST = "strtw.herokuapp.com"; // Or where-ever you deployed your API
-String PATH = "nasezero";
+String PATH = "YourTwitterStatusHere";
 const int HTTPS_PORT = 443;
-const int8_t NUM_PIXELS = 26;
+const int8_t NUM_PIXELS = 100;
 int colors[NUM_PIXELS];
 int8_t pixelToLight;
 String message;
 DynamicJsonBuffer jsonBuffer;
 const int ERROR_MESSAGE_SIZE = 12;
-const int ERROR_MESSAGE[12] = {13, 14, 19, 2,  14, 13,
-                               13, 4,  2,  19, 4,  3}; // "notconnected"
+const int ERROR_MESSAGE[12] = {13, 14, 19, 2, 14, 13,
+                               13, 4, 2, 19, 4, 3}; // "notconnected"
+
+int mode = DISPLAY_MESSAGE_MODE;
 
 Adafruit_NeoPixel strip =
-    Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  pinMode(BUTTON_PIN_GREEN, INPUT);
+  pinMode(BUTTON_PIN_RED, INPUT);
+
   initColors();
 
   strip.begin();
   strip.show();
 
   Serial.begin(9600);
+
   Serial.printf("Connecting to %s ", SSID);
   WiFi.begin(SSID, PASSWORD);
-
   Serial.println(" connected");
 }
 
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
-    colorWipe();
     lightErrorMessage();
     Serial.print(".");
+  } else {
     colorWipe();
-    displayMessage();
+    logDeviceData();
+
+    Serial.print("mode: ");
+    Serial.println(mode);
+
+    switch (mode) {
+    case DISPLAY_MESSAGE_MODE:
+      displayMessage();
+      break;
+    case RAINBOW_MODE:
+      rainbow(50);
+      break;
+    }
   }
 }
 
@@ -54,13 +75,15 @@ void displayMessage() {
     client.print(String("GET /" + PATH) + " HTTP/1.1\r\n" + "Host: " + HOST +
                  "\r\n" + "Connection: close\r\n" + "\r\n");
 
-    while (client.connected()) {
+    while (client.connected() && mode == DISPLAY_MESSAGE_MODE) {
       if (client.available()) {
         message = client.readStringUntil('\n');
         JsonArray &pixelsArray = jsonBuffer.parseArray(message);
 
         for (auto value : pixelsArray) {
-          Serial.println(value.as<char *>());
+          checkButtonStates();
+          if (mode != DISPLAY_MESSAGE_MODE)
+            break;
           const int letterPosition = value.as<int>();
           if (!PRECISE) {
             lightLetter(letterPosition);
@@ -79,6 +102,14 @@ void displayMessage() {
   delay(300);
 }
 
+void checkButtonStates() {
+  if (digitalRead(BUTTON_PIN_GREEN) == LOW) {
+    mode = DISPLAY_MESSAGE_MODE;
+  } else if (digitalRead(BUTTON_PIN_RED) == LOW) {
+    mode = RAINBOW_MODE;
+  }
+}
+
 void lightLetter(int pixel) {
   if (pixel >= 8 && pixel <= 16) {
     pixelToLight = pixel + 1;
@@ -91,14 +122,22 @@ void lightLetter(int pixel) {
   delay(200);
   strip.setPixelColor(pixelToLight, colors[pixelToLight]);
   strip.show();
-  delay(1050);
+
+  // Lazy way of checking button states while keeping paused for 1050ms
+  delay(300);
+  checkButtonStates();
+  delay(250);
+  checkButtonStates();
+  delay(250);
+  checkButtonStates();
+  delay(250);
+
   strip.setPixelColor(pixelToLight, 0);
   strip.show();
 }
 
 // Change each letter to the actual index of that letter's light position
 void lightPreciseLetter(int pixel) {
-  Serial.println(pixel);
   if (pixel == 0) {
     pixelToLight = 62; // a
   } else if (pixel == 1) {
@@ -152,6 +191,26 @@ void lightPreciseLetter(int pixel) {
   } else if (pixel == 25) {
     pixelToLight = 1; // z
   }
+
+  Serial.println(pixelToLight);
+
+  strip.setPixelColor(pixelToLight, 0);
+  strip.show();
+  delay(200);
+  strip.setPixelColor(pixelToLight, colors[pixelToLight]);
+  strip.show();
+
+  // Lazy way of checking button states while keeping paused for 1050ms
+  delay(300);
+  checkButtonStates();
+  delay(250);
+  checkButtonStates();
+  delay(250);
+  checkButtonStates();
+  delay(250);
+
+  strip.setPixelColor(pixelToLight, 0);
+  strip.show();
 }
 
 void lightAll() {
@@ -163,6 +222,8 @@ void lightAll() {
 
 void lightErrorMessage() {
   for (int i = 0; i < ERROR_MESSAGE_SIZE; i++) {
+    if (WiFi.status() == WL_CONNECTED || mode != DISPLAY_MESSAGE_MODE)
+      break;
     if (!PRECISE) {
       lightLetter(ERROR_MESSAGE[i]);
     } else {
@@ -199,4 +260,47 @@ void initColors() {
       break;
     }
   }
+}
+
+void logDeviceData() {
+  Serial.print("Device IP: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Device MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  Serial.print("connecting to ");
+  Serial.println(HOST);
+}
+
+// From Adafruit NeoPixel example
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    if (mode != RAINBOW_MODE)
+      break;
+    for (i = 0; i < strip.numPixels(); i++) {
+      checkButtonStates();
+      if (mode != RAINBOW_MODE)
+        break;
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// From Adafruit NeoPixel example
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
