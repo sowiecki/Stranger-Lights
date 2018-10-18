c28b2e8f42d8139d2acdd9ca8ea221adbff10e07
sowiecki <nase00@gmail.com>
2018-10-18 18:28:21 -0500

    Fix recent tweet ordering from the API.

--- constants.js
+++ constants.js
@@ -3,3 +3,5 @@ module.exports.PORT = process.env.PORT || 3000;
 module.exports.DEFAULT_MESSAGE = process.env.DEFAULT_MESSAGE || 'run';
 
 module.exports.TWEET_EXPIRED = 180;
+
+module.exports.TWITTER_DATE_FORMAT = 'ddd MMM DD HH:mm:ss +ZZ YYYY';
--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const moment = require('moment');
 const finalhandler = require('finalhandler');
 const Router = require('router');
 const Twitter = require('twitter');
+const { isEmpty } = require('lodash');
 
 const ENV = require('./env');
-const { PORT, DEFAULT_MESSAGE, TWEET_EXPIRED } = require('./constants');
+const {
+  PORT,
+  DEFAULT_MESSAGE,
+  TWEET_EXPIRED,
+  TWITTER_DATE_FORMAT
+} = require('./constants');
 const {
   convertTweetToLetterIndexes,
-  getTime,
   getTimeSinceLastTweet,
   getScreenName
 } = require('./utils');
@@ -18,39 +23,57 @@ const client = new Twitter(ENV.twitter);
 
 const state = {
   timeSinceLastTweet: moment().subtract(TWEET_EXPIRED, 'seconds'),
-  storedTweet: { text: 'RUN' }
+  storedTweets: { default: { text: 'RUN' } }
 };
+
+const filterStatuses = statuses =>
+  statuses
+    .filter(
+      status => status.retweet_count === 0 && isEmpty(status.retweeted_status)
+    )
+    .sort((a, b) => {
+      return moment(a.created_at, TWITTER_DATE_FORMAT).isBefore(
+        moment(b.created_at, TWITTER_DATE_FORMAT)
+      );
+    });
 
 const getLatestTweet = (res, req) => {
   const screenName = getScreenName(res, req);
 
-  client.get('search/tweets', { q: screenName }, (error, tweets, response) => {
-    const latestTweet =
-      tweets.statuses && tweets.statuses.length > 0
-        ? tweets.statuses[0]
-        : { text: DEFAULT_MESSAGE };
+  client.get(
+    'search/tweets',
+    { q: screenName, result_type: 'recent' },
+    (error, tweets, response) => {
+      const filteredStatuses = filterStatuses(tweets.statuses);
+      const latestTweet =
+        filteredStatuses && filteredStatuses.length > 0
+          ? filteredStatuses[0]
+          : { text: DEFAULT_MESSAGE };
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(convertTweetToLetterIndexes(latestTweet, screenName));
+      res.setHeader('Content-Type', 'application/json');
+      res.end(convertTweetToLetterIndexes(latestTweet, screenName));
 
-    state.storedTweet = latestTweet;
-  });
+      state.storedTweets[screenName] = latestTweet;
+    }
+  );
 };
 
 const run = () => {
   router.get('/:screen_name?', (req, res) => {
-    const storedTweetIsExpired = getTime().isAfter(
+    const screenName = getScreenName(res, req);
+    const cacheCounterExpired = moment().isAfter(
       moment(state.timeSinceLastTweet).add(TWEET_EXPIRED, 'seconds')
     );
 
-    if (storedTweetIsExpired) {
-      state.timeSinceLastTweet = getTime();
+    if (cacheCounterExpired || !state.storedTweets[screenName]) {
+      state.timeSinceLastTweet = moment();
 
       getLatestTweet(res, req);
     } else {
-      const screenName = getScreenName(res, req);
       res.setHeader('Content-Type', 'application/json');
-      res.end(convertTweetToLetterIndexes(state.storedTweet, screenName));
+      res.end(
+        convertTweetToLetterIndexes(state.storedTweets[screenName], screenName)
+      );
     }
   });
 
